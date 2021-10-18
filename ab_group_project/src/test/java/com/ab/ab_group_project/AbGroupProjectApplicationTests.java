ebb11a9342e0776de178e948891ed5a0ae3b0483
magnussanne <magnussanne@me.com>
2021-10-18 15:49:59 +0100

    Basic structure of project

---
+++ ab_group_project/src/test/java/com/ab/ab_group_project/AbGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.ab_group_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbGroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

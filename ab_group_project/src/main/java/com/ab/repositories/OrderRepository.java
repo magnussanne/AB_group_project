09ac4feaf9142fd15fc223de57e4d25bf4e40329
sabahahmadd <90405836+sabahahmadd@users.noreply.github.com>
2021-10-19 14:50:23 +0100

    SELECT statements for user and order repository

--- ab_group_project/src/main/java/com/ab/repositories/OrderRepository.java
+++ ab_group_project/src/main/java/com/ab/repositories/OrderRepository.java
@@ -1,11 +1,14 @@
 package com.ab.repositories;
-
 import com.ab.entities.Orders;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, Integer> {
 
+	@Query("SELECT o from Orders o WHERE o.users.userId =:uId")
+	public List<Orders> ordersFromUser(@Param("uId") int userId);
 }
--- ab_group_project/src/main/java/com/ab/repositories/UserRepository.java
+++ ab_group_project/src/main/java/com/ab/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package com.ab.repositories;
-
 import com.ab.entities.Users;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
 
+	@Query("SELECT u from Users u WHERE u.username =:uName and u.password =:password")
+	public Users loginCheck(@Param("uName") String username,@Param("password") String password);
 }

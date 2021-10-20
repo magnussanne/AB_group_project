067f87111eb8070d1191a4967ec7820539f74431
magnussanne <magnussanne@me.com>
2021-10-20 12:17:25 +0100

    security config creation

---
+++ ab_group_project/src/main/java/com/ab/configs/SpringSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.ab.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication().withUser("kishore").password(passwordEncoder().encode("1234")).roles("USER").and()
+                .withUser("admin").password(passwordEncoder().encode("1111")).roles("ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().antMatchers("/admin").hasAnyRole("ADMIN").antMatchers("/user")
+                .hasAnyRole("USER", "ADMIN").antMatchers("/").permitAll().and().formLogin();
+
+    }
+
+}

e687f166a7af425db0d0a699a06c0738ac9b3a55
sabahahmadd <90405836+sabahahmadd@users.noreply.github.com>
2021-10-20 11:13:43 +0100

    @service added to service files

--- ab_group_project/src/main/java/com/ab/services/OrderService.java
+++ ab_group_project/src/main/java/com/ab/services/OrderService.java
@@ -1,9 +1,11 @@
 package com.ab.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.ab.entities.Orders;
 import com.ab.repositories.OrderRepository;
 
+@Service
 public class OrderService {
 	
 	@Autowired
--- ab_group_project/src/main/java/com/ab/services/UserService.java
+++ ab_group_project/src/main/java/com/ab/services/UserService.java
@@ -1,8 +1,10 @@
 package com.ab.services;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.ab.entities.Users;
 import com.ab.repositories.UserRepository;
 
+@Service
 public class UserService {
 	
 	@Autowired

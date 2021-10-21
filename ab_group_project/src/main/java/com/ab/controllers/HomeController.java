0419164ed1f7800831e7d53b64854147bc930a2e
noorbaakza <55815113+noorbaakza@users.noreply.github.com>
2021-10-21 11:10:39 +0100

    Add files via upload

--- ab_group_project/src/main/java/com/ab/controllers/HomeController.java
+++ ab_group_project/src/main/java/com/ab/controllers/HomeController.java
@@ -1,8 +1,15 @@
 package com.ab.controllers;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import com.ab.entities.Users;
+
+@Controller
 public class HomeController {
 
     @GetMapping("/")
@@ -10,5 +17,36 @@ public class HomeController {
         return "home";
 
     }
+    
+    @RequestMapping(value="/registration", method=RequestMethod.GET)
+    public String getRegistrationScreen() {
+  	
+    	return "registration";
 
+  }
+    
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public String postRegistration(@RequestParam("email") String email, @RequestParam("username") String username, @RequestParam("password") String password) {
+        
+    	Users user = new Users(email, username, password, null);
+    	
+    	return "login";
+
+    }
+    
+    @RequestMapping(value="/login", method=RequestMethod.GET)
+    public String getLoginScreen() {
+  	
+    	return "login";
+
+  }
+    
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String postLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
+        
+    	Users user = new Users(null, username, password, null);
+    	
+    	return "home";
+
+    }
 }

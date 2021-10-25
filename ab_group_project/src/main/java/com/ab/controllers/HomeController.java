9bd8c25bceeda984e6622d56e9632dfbfdb99497
sabahahmadd <90405836+sabahahmadd@users.noreply.github.com>
2021-10-25 13:24:15 +0100

    changes

--- ab_group_project/src/main/java/com/ab/controllers/HomeController.java
+++ ab_group_project/src/main/java/com/ab/controllers/HomeController.java
@@ -1,21 +1,14 @@
 package com.ab.controllers;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @RequestMapping(value="/", method=RequestMethod.GET)
+    @GetMapping("/")
     public String getHome() {
         return "home";
 
     }
 
-    @RequestMapping(value="/dashboard", method=RequestMethod.GET)
-    public String displayOptions(){
-		return "dashboard";
-	}
 }
--- ab_group_project/src/main/java/com/ab/controllers/UserController.java
+++ ab_group_project/src/main/java/com/ab/controllers/UserController.java
@@ -23,10 +23,10 @@ public class UserController {
 
 	
 	// options being listed from user's dashboard
-	/*
-	 * @GetMapping("/dashboard") public String displayOptions(){ return "dashboard";
-	 * }
-	 */
+	@GetMapping("/dashboard")
+	public String displayOptions(){
+		return "dashboard";
+	}
 	
 	// when user selects option to view all their orders
 	@PostMapping("/dashboard")

0b4c4981440fc6dfb4c2bdbd68e8e5cc346cf931
sabahahmadd <90405836+sabahahmadd@users.noreply.github.com>
2021-10-25 14:22:06 +0100

    changes

--- ab_group_project/src/main/java/com/ab/controllers/HomeController.java
+++ ab_group_project/src/main/java/com/ab/controllers/HomeController.java
@@ -1,21 +1,12 @@
 package com.ab.controllers;
-<<<<<<< HEAD
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
->>>>>>> parent of 3ddfc64 (changes to html and css)
 
-@RestController
+@Controller
 public class HomeController {
 
-<<<<<<< HEAD
     @RequestMapping(value="/", method = RequestMethod.GET)
-=======
-    @GetMapping("/")
->>>>>>> parent of 3ddfc64 (changes to html and css)
     public String getHome() {
         return "home";
 

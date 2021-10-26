3ce58434072b4c13723e161d0c09f8a52540ec78
sabahahmadd <90405836+sabahahmadd@users.noreply.github.com>
2021-10-26 13:00:15 +0100

    restful controller changed to controller

--- ab_group_project/src/main/java/com/ab/controllers/HomeController.java
+++ ab_group_project/src/main/java/com/ab/controllers/HomeController.java
@@ -1,11 +1,12 @@
 package com.ab.controllers;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @RequestMapping(value="/",method=RequestMethod.GET)
     public String getHome() {
         return "home";
 

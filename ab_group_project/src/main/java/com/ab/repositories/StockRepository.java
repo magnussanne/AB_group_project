2f10f3cc3e1e214484e04eb51fba0df5691396c9
sabahahmadd <90405836+sabahahmadd@users.noreply.github.com>
2021-10-29 10:59:53 +0100

    stock repository query working

--- ab_group_project/src/main/java/com/ab/repositories/StockRepository.java
+++ ab_group_project/src/main/java/com/ab/repositories/StockRepository.java
@@ -1,14 +1,13 @@
 package com.ab.repositories;
-
 import com.ab.entities.Stocks;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stocks, Integer> {
 
-    // @Query("SELECT o from Stocks o WHERE o.stock.stockId =:sId")
-    // public Stocks findStock(@Param("sId") int stockId);
-
+    @Query("SELECT s from Stocks s WHERE s.name =:sName")
+    public Stocks findStock(@Param("sName") String name);
 }
